2c76f40b611984e77da43d09a210e0325442c64a
Angela <amr25@berkeley.edu>
2023-10-14 18:01:38 -0500

    added Health + Charge + Perry

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform player;
+    private Vector2 target;
+    [SerializeField] private float damage = 5;
+    [SerializeField] private Health doofHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y + 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            doofHealth.curHealth -= damage;
+            DestroyItem();
+        }
+    }
+
+    #region On Contact With Player
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyItem();
+        }
+    }
+
+    void DestroyItem()
+    {
+        doofHealth.curHealth -= damage;
+        doofHealth.SetHealth(doofHealth.curHealth);
+        Destroy(gameObject);
+    }
+
+    #endregion
+}
